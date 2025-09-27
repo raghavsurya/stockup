f802a50e232cb3a07b7a87c056644a1dc6a069ac
Raghav Surya <raghav.surya@outlook.com>
2025-09-27 09:35:09 +0100

    fix linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """ Add two numbers"""
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from . import calc
 
+
 class CalcTest(SimpleTestCase):
     def test_add_two_nums(self):
         res = calc.add(5, 7)
